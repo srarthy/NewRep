2c6dfbf6f3c3c0d61785ab04e7a619994e9c5b77
Arthy <Arthy@DESKTOP-L4HUAOJ>
2020-08-23 09:30:31 -0700

    One line added

--- P1/src/test.java
+++ P1/src/test.java
@@ -4,7 +4,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("welcome to Java"); 
-       System.out.println("New to Java. Learning a lot.Now learn......"); 
+       System.out.println("New to Java. Learning a lot.Now learn11111......"); 
+       System.out.println("Dare to make mistakes......"); 
 	}
 
 }
