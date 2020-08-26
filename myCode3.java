4d5f7cbe505a4df0813f77ed4ccaa46bf639610a
srarthy <64612018+srarthy@users.noreply.github.com>
2020-08-26 16:03:42 -0700

    Update myCode3.java

--- myCode3.java
+++ myCode3.java
@@ -10,6 +10,7 @@ public class myCode3 {
 		System.out.println("Another addition by Arthy.....");
 		System.out.println("Last line added");
 		System.out.println("New Last line added now.See you soon.Love you............");
+		System.out.println("Hurray.........................");
 	}
 
 }
